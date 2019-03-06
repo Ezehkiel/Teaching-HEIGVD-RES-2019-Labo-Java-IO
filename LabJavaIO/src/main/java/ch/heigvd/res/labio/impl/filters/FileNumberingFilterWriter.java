1a741823789e8c14e6bc438e627fabdf8d491c11
Remi Poulard <remi.poulard@heig-vd.ch>
2019-03-06 11:57:09 +0100

    Numbering done with passed test

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -16,6 +16,12 @@ import java.util.logging.Logger;
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
+  private int compteurLigne = 1;
+  private int lineCounter = 1;
+  private int previousChar = -1;
+  private static final char rSeparator = '\r';
+  private static final char nSeparator = '\n';
+  private static final String tSeparator = "\t";
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
@@ -25,17 +31,39 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    this.write(str.toCharArray(), off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for (int i = off; i < off + len; i++){
+      this.write(cbuf[i]);
+    }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    // Check if this is the first line
+    if (lineCounter == 1){
+      out.write(lineCounter + tSeparator);
+      out.write(c);
+      lineCounter++;
+    } else {
+      // Check for the MACOS separator
+      if (previousChar == rSeparator && c != nSeparator) {
+        out.write(lineCounter + tSeparator);
+        lineCounter++;
+      }
 
+      out.write(c);
+
+      // Check if we have a Unix / Windows new line
+      if (c == nSeparator){
+        out.write(lineCounter + tSeparator);
+        lineCounter++;
+      }
+    }
+    // update previous char to further check if it is the MACOS separator
+    previousChar = c;
+  }
 }
